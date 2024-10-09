d0194438d4c76cf865ab24e1cc45b494c187af26
Carlos Eduardo Casteliano <carlosecdpaula@gmail.com>
2024-10-09 00:05:19 -0300

    feat: correção em layout

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <div class="container mx-auto p-4 my-10">
     <About />
     <div class="mt-10">
-      <h2 class="text-3xl font-bold mb-4 text-center">Meus Projetos</h2>
+      <!-- <h2 class="text-3xl font-bold mb-4 text-center">Meus Projetos</h2> -->
       <div class="flex flex-wrap justify-center">
         <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
       </div>
     </div>
-    <div class="mt-10">
+    <div class="mt-12">
       <h2 class="text-3xl font-bold mb-4 text-center">Entre em Contato</h2>
       <ContactForm />
     </div>
@@ -15,7 +15,6 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue'
 import About from '@/components/About.vue';
 import ProjectCard from '@/components/ProjectCard.vue';
@@ -24,39 +23,19 @@ import ContactForm from '@/components/ContactForm.vue';
 const projects = ref([
   {
     id: 1,
-    name: 'Projeto 1',
+    name: 'Produção Mecânico Web',
     description: 'Este é um projeto incrível. Outro projeto fantástico. Outro projeto fantástico. Outro projeto fantástico.',
-    website: 'https://projeto1.com',
-    github: 'https://github.com/seuusuario/projeto1',
+    website: 'https://producaomecanico.carloscasteliano.com.br',
+    github: 'https://github.com/Castelianoads/Producao-Mecanico-Web',
   },
   {
     id: 2,
-    name: 'Projeto 2',
+    name: 'Produção Mecânico App',
     description: 'Outro projeto fantástico. Outro projeto fantástico. Outro projeto fantástico.',
-    website: 'https://projeto2.com',
-    github: 'https://github.com/seuusuario/projeto2',
+    website: 'https://carloscasteliano.com.br/producaomecanico',
+    github: 'https://github.com/Castelianoads/Producao-Mecanico-App-Flutter',
   },
-  {
-    id: 2,
-    name: 'Projeto 2',
-    description: 'Outro projeto fantástico. Outro projeto fantástico. Outro projeto fantástico.',
-    website: 'https://projeto2.com',
-    github: 'https://github.com/seuusuario/projeto2',
-  },
-  {
-    id: 2,
-    name: 'Projeto 2',
-    description: 'Outro projeto fantástico. Outro projeto fantástico. Outro projeto fantástico.',
-    website: 'https://projeto2.com',
-    github: 'https://github.com/seuusuario/projeto2',
-  },
-  {
-    id: 2,
-    name: 'Projeto 2',
-    description: 'Outro projeto fantástico. Outro projeto fantástico. Outro projeto fantástico.',
-    website: 'https://projeto2.com',
-    github: 'https://github.com/seuusuario/projeto2',
-  },
+
 ])
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Importa o ícone do LinkedIn
+
+library.add(faLinkedin);
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
