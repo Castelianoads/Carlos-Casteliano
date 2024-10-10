f5bda9f673ca84e9bae44d3b8ff080fb0afebd61
Carlos Eduardo Casteliano <carlosecdpaula@gmail.com>
2024-10-09 21:08:41 -0300

    fix: COrreções nos icons da rede social e email

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="flex flex-col md:flex-row items-center justify-between gap-8">
-    <!-- Imagem -->
     <div class="md:w-1/2 flex justify-center">
       <img src="../assets/carlos_img.jpg.jpg" alt="Minha Foto" class="rounded-full w-48 h-48 object-cover" />
     </div>
 
-    <!-- Texto e Ícones -->
     <div class="md:w-1/2 text-center md:text-left">
-      <!-- Nome Responsivo -->
       <h1 class="text-4xl font-bold mb-4 break-words md:whitespace-nowrap">
         Carlos Eduardo Casteliano
       </h1>
@@ -20,51 +17,86 @@
         Em desenvolvimento
       </p>
 
-
-      <!-- Ícones de Redes Sociais -->
       <div class="flex justify-center md:justify-start space-x-6 mt-4">
-        <!-- Site -->
-        <a href="https://www.carloscasteliano.com.br" target="_blank" aria-label="Website">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-700 hover:text-blue-400" fill="none"
-            viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M12 3c2.21 0 4 .895 5.333 2.354A8.001 8.001 0 0112 21a8.001 8.001 0 01-5.333-15.646A7.974 7.974 0 0112 3z" />
-          </svg>
-        </a>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <a href="https://www.linkedin.com/in/carlos-eduardo-casteliano/" target="_blank" aria-label="LinkedIn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" class="hover:text-gray-500"
+                  viewBox="0 0 24 24">
+                  <path fill="currentColor"
+                    d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z" />
+                </svg>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>LinkedIn</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
 
-        <!-- LinkedIn -->
-        <a href="https://www.linkedin.com/in/carlos-eduardo-casteliano/" target="_blank" aria-label="LinkedIn">
-          <font-awesome-icon :icon="['fab', 'linkedin']" size="2x" />
-        </a>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <a href="https://github.com/castelianoads" target="_blank" aria-label="GitHub">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" class="hover:text-gray-500"
+                  viewBox="0 0 24 24">
+                  <path fill="currentColor"
+                    d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2" />
+                </svg>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>GitHub</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
 
-        <!-- GitHub -->
-        <a href="https://github.com/carloscastelianoads" target="_blank" aria-label="GitHub">
-          <font-awesome-icon :icon="['fab', 'github']" size="lg" />
-        </a>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <a href="https://dev.azure.com/casteliano" target="_blank" aria-label="Azure DevOps">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" class="hover:text-gray-500"
+                  viewBox="0 0 128 128">
+                  <path fill="currentColor"
+                    d="M120.89 28.445v69.262l-28.445 23.324l-44.09-16.07v15.93L23.395 88.25l72.746 5.688V31.574ZM96.64 31.93L55.82 7.11v16.285L18.348 34.418L7.109 48.852v32.785l16.075 7.11V46.718Zm0 0" />
+                </svg>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Azure DevOps</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
 
-        <!-- Azure DevOps -->
-        <a href="https://dev.azure.com/seuprojeto" target="_blank" aria-label="Azure DevOps">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-700 hover:text-blue-500" fill="none"
-            viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M12 1.5a9 9 0 11-4.38 16.928l-3.735 1.166a.75.75 0 01-.95-.952l1.166-3.734A9 9 0 0112 1.5z" />
-          </svg>
-        </a>
-
-        <!-- Gmail -->
-        <a href="mailto:contato@carloscasteliano.com.br" aria-label="Gmail">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-700 hover:text-red-500" fill="none"
-            viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M3.75 4.5h16.5A1.5 1.5 0 0121.75 6v12a1.5 1.5 0 01-1.5 1.5H3.75A1.5 1.5 0 012.25 18V6a1.5 1.5 0 011.5-1.5zM21 6l-9 6-9-6" />
-          </svg>
-        </a>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <a href="mailto:contato@carloscasteliano.com.br" aria-label="Gmail">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" class="hover:text-gray-500"
+                  viewBox="0 0 24 24">
+                  <path fill="currentColor"
+                    d="M20 18h-2V9.25L12 13L6 9.25V18H4V6h1.2l6.8 4.25L18.8 6H20m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2" />
+                </svg>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Email</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip';
 
 </script>
 
