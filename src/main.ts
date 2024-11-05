65ad31d1d5dd8a871429e046a8b327c5510bb8c2
Carlos Eduardo Casteliano <carlosecdpaula@gmail.com>
2024-11-05 13:02:53 -0300

    Implementação de tag do google com Vue Gtag

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import './assets/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import VueGtag from "vue-gtag";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Importa o ícone do LinkedIn
@@ -12,6 +12,13 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Importa o í
 library.add(faLinkedin);
 
 const app = createApp(App)
+
+console.log(import.meta.env.MODE)
+if(import.meta.env.MODE === 'production'){
+  app.use(VueGtag, {
+    config: { id: "G-MR8E3B3CQP" }
+  });
+}
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
